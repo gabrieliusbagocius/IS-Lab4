07cd27dd97bb4722c76faeb87c4934b9be36c9cc
Gabriel <54512419+gabrieliusbagocius@users.noreply.github.com>
2020-01-12 00:22:09 +0200

    C#
    Worked well but only with paint file, otherwise wouldn't register the image properly. Used Tesseract libraries with downloaded trained data.

---
+++ Laboratory4/Laboratory4/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace Test
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var img = Pix.LoadFromFile(@"C:\Users\navi\source\repos\Laboratory4\tst2.png");
+            TesseractEngine engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default);
+            Page page = engine.Process(img, PageSegMode.Auto);
+            string result = page.GetText();
+            Console.WriteLine(result);
+        }
+    }
+}
